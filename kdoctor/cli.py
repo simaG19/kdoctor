81b36de526c07dc1e7ef69ce8db8adcf31439f8a
Simon Wubshet Bekele <simonwubshet254@gmail.com>
2026-07-16 16:41:53 +0300

    feat(nodes): format status output using Rich tables

--- kdoctor/cli.py
+++ kdoctor/cli.py
@@ -1,7 +1,9 @@
 import typer
+from rich.console import Console
+from rich.table import Table
 from kdoctor.kube import KubeClient
-
 app = typer.Typer()
+console = Console()
 
 @app.command()
 def version():
@@ -14,17 +16,31 @@ def version():
 @app.command()
 def nodes():
     kube = KubeClient()
-
     nodes = kube.get_nodes()
+    
+    table = Table(title="Kubernetes Node Registry")
+    
+    table.add_column("Node Name", style="cyan", no_wrap=True)
+    table.add_column("Status", justify="center")
 
     for node in nodes.items:
+        node_name = node.metadata.name
         ready_status ="unknown"
+        status_color = "yellow"
+
         for condition in node.status.conditions:
             if condition.type == "Ready":
-                ready_status= condition.type
+                if condition.status == "True":
+                    ready_status = "Ready"
+                    status_color = "green"
+                else:
+                    ready_status = "NotReady"
+                    status_color = "red"
                 break
-                print(node.metadata.name, ready_status)
-    
+
+                
+        table.add_row(node_name, f"[{status_color}]{ready_status}[/{status_color}]")
+    console.print(table)        
 
 
 if __name__ == "__main__":
